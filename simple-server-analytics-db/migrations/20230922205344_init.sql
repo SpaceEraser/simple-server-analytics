ba217e2be49b05797eb6e44c1af40c193772a13d
SpaceEraser <2659641+SpaceEraser@users.noreply.github.com>
2023-10-02 21:24:44 -0400

    Fix more errors in migration

--- simple-server-analytics-db/migrations/20230922205344_init.sql
+++ simple-server-analytics-db/migrations/20230922205344_init.sql
@@ -10,18 +10,18 @@ CREATE TABLE "sa_connection" (
 CREATE TABLE "sa_request" (
     "id" BLOB NOT NULL PRIMARY KEY,
     "created_at" DATETIME NOT NULL,
-    "conn_id" BLOB NULL REFERENCES "connection" ("id") ON DELETE SET NULL,
-    "hostname" TEXT NOT NULL,
+    "conn_id" BLOB NULL REFERENCES "sa_connection" ("id") ON DELETE SET NULL,
+    "method" TEXT NOT NULL,
     "path" TEXT NOT NULL,
-    "user_agent" TEXT NOT NULL,
-    "method" TEXT NOT NULL
+    "hostname" TEXT NOT NULL,
+    "user_agent" TEXT NOT NULL
 );
 
 CREATE TABLE "sa_response" (
     "id" BLOB NOT NULL PRIMARY KEY,
     "created_at" DATETIME NOT NULL,
-    "conn_id" BLOB NULL REFERENCES "connection" ("id") ON DELETE SET NULL,
-    "req_id" BLOB NOT NULL REFERENCES "request" ("id") ON DELETE SET NULL,
+    "conn_id" BLOB NULL REFERENCES "sa_connection" ("id") ON DELETE SET NULL,
+    "req_id" BLOB NOT NULL REFERENCES "sa_request" ("id") ON DELETE SET NULL,
     "duration" TEXT NOT NULl,
     "status" INT NOT NULL
 );
